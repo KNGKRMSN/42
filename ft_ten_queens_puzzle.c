161afc89e6e164d617a4a074a5175437a807eb6f
KNGKRMSN <54447514+KNGKRMSN@users.noreply.github.com>
2021-08-16 18:09:24 +0200

    Add files via upload

--- ft_ten_queens_puzzle.c
+++ ft_ten_queens_puzzle.c
@@ -1,73 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ten_queens_puzzle.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mprud-ho <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/12 13:24:51 by mprud-ho          #+#    #+#             */
+/*   Updated: 2021/08/16 18:04:23 by mprud-ho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 
-int	ft_check(tab, i)
+int	ft_check_diags(int *tab, int x)
 {
-	int	diago;
-	int	y;
-	int	line;
+	int	i;
 
-	diago = 1;
-	k = 0;
-	line = 0;
-	while (line < i)
+	i = 1;
+	while (i < x)
 	{
-		if (tab[i] == tab[line])
+		if (tab[x - i] == tab[x] + i)
 			return (0);
-		line++;
-	}
-	while
-		if (tab[i - diago] == k - diago)
-
-}
-
-void	ft_may_i(tab)
-{
-	int i;
-	int k;
-	int check;
-	
-	i = 0;
-	if (!(tab[i - 1]))
-	{
-		tab[i] = 0;
 		i++;
 	}
-	if (tab[i - 1])//ca marche pas ton if
+	i = 1;
+	while (i > x)
 	{
-		k = 0;
-		while (k <= 9)
+		if (tab[x - i] == tab[x] - i)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_check_line(int *tab, int x)
+{
+		int	i;
+
+		i = 0;
+		if (x == 0)
+			return (1);
+		while (tab[i] != tab[x] && i < x) //essayer sans le !=
 		{
-			if (tab[i - 1] != k && tab[i - 1] != k - 1 && tab[i - 1] != k + 1)
-			{
-				check = 0;
-				while (check < i)
-				{
-					if (tab[check] == k)
-						maxicheck
-				}
-				tab[i] = k;
-				break ;
-			}
-			else
-				k++;
+			if (tab[i] == tab[x])
+				return (0);
+			i++;
 		}
-		i++;
-	}
+		return (1);
 }
 
-void	ft_ten_queens_puzzle(void)
+int	ft_recursive(int *tab, int pos)
 {
-	int	*tab[10];
 	int	x;
 	int	y;
-	int	pos;
-	
-	x = 0;
-	y = 0;
-	
-	ft_may_i(tab);
+
+
 	if (pos == 10)
 	{
-		ft_print(tab);
+		x = -1;
+		while (++x < 10)
+			write(1, tab[x] + 48, 1);
+		write(1, "\n", 1);
 	}
+	x = pos;
+	if (tab[x] != -1)
+		return (ft_recursive(tab, pos + 1));
+	y = -1;
+	while (++y <= 9)
+	{
+		if (ft_check_diags(tab, x) && ft_check_line(tab, x))
+		{
+			tab[x] = y;
+			if (ft_recursive(tab, pos + 1))
+				return (1);
+		}
+	}
+	tab[x] = 0;
+	return (0);
+}
+
+int	ft_ten_queens_puzzle(void)
+{
+	int		tab[10];
+	int		pos;
+	int		i;
+
+	i = -1;
+	pos = (0);
+	while (++i <= 10)
+		tab[i] = -1;
+	ft_recursive(tab, pos);
+	return (724);
+}
+
+int main()
+{
+	ft_ten_queens_puzzle();
 }
